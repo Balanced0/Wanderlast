8b9f9d69ffa33a3100d524295ce4296c15c07dee
balanced0 <alvichowdhury013@gmail.com>
2026-05-15 23:49:24 +0600

    feat: Add destination management features with new pages and components
    - Implemented AddDestinationPage for adding new travel destinations.
    - Created DestinationDetailsPage for displaying detailed information about a specific destination.
    - Developed DestinationPage to list all available destinations.
    - Added Banner component for the homepage to promote travel experiences.
    - Introduced DestinationCard component for individual destination display.
    - Created Navbar and Footer components for consistent site navigation and information.
    - Added various assets including images for destinations and UI elements.
    - Updated package.json with new dependencies for UI components and styling.
    - Refactored global styles to integrate new design system.

---
+++ src/app/destinations/[id]/page.jsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import { LuMapPin } from "react-icons/lu";
+import { FaRegCalendar } from "react-icons/fa6";
+
+const DestinationDetailsPage = async ({ params }) => {
+  const { id } = await params;
+
+  const res = await fetch(`http://localhost:5000/destination/${id}`);
+  const destination = await res.json();
+  const { imageUrl, price, destinationName, duration, country, description } =
+    destination;
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Image
+        alt={destinationName}
+        src={imageUrl}
+        width={500}
+        height={800}
+      ></Image>
+      <div className="p-2">
+        <div className="flex items-center gap-1">
+          <LuMapPin /> <span>{country}</span>
+        </div>
+        <div className="flex justify-between">
+          <div>
+            <div>
+              <h2 className="text-xl font-bold">{destinationName}</h2>
+            </div>
+            <div className="flex gap-1 items-center">
+              <FaRegCalendar /> {duration}
+            </div>
+          </div>
+
+          <div>
+            <h3 className="text-2xl font-bold">$ {price}</h3>
+          </div>
+        </div>
+        <h1 className="text-2xl font-bold mt-10">Overview</h1>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DestinationDetailsPage;
